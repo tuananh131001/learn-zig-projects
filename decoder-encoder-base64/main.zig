30db6b96f77ac76cb431096ba1df68cf4f58a4a7
anhnt <tuananh131001@gmail.com>
2025-10-28 22:07:13 +0700

    Calculate encode length

--- decoder-encoder-base64/main.zig
+++ decoder-encoder-base64/main.zig
@@ -15,9 +15,27 @@ const Base64 = struct {
     pub fn _char_at(self: Base64, idx: usize) u8 {
         return self._table[idx];
     }
+    
+
 };
+
+pub fn _cal_encode_length(input: []const u8) !usize {
+    // return 4 if length is 3
+    //
+    if (input.len < 3) {
+        return 4;
+    }
+
+    // divide the length by 3 then apply ceil then multiple by 4
+    const n_group: usize = try std.math.divCeil(usize, input.len, 3);
+
+    return n_group * 4;
+}
 
 pub fn main() !void {
     const base64 = Base64.init();
+    const arr = "ABCDCISOS";
     std.debug.print("Character at 28: {c}", .{base64._char_at(28)});
+    const len: usize = try _cal_encode_length(arr);
+    std.debug.print("Size: {}", .{len});
 }
